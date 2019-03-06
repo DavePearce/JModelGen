4603c3be49acfb8330135e5e660560e821869b1d
DavePearce <david.pearce@ecs.vuw.ac.nz>
2019-03-06 18:43:50 +1300

    Add Domains.Constrained
    This adds a "constrained" domain which attempts to select items matching
    a given predicate.  This is not particularly efficient.  However, it
    does at least avoid holding the entire domain in memory at any given
    moment.  Instead, it just holds the indices of matching items.

--- src/jmodelgen/util/Domains.java
+++ src/jmodelgen/util/Domains.java
@@ -1,7 +1,9 @@
 package jmodelgen.util;
 
+import java.util.ArrayList;
 import java.util.Random;
 import java.util.function.Consumer;
+import java.util.function.Predicate;
 
 import jmodelgen.core.Domain;
 
@@ -29,8 +31,25 @@ public class Domains {
 		}
 	}
 
-
-
+	/**
+	 * Apply a given lambda consumer to all elements matching a given condition of a
+	 * domain.
+	 *
+	 * @param domain
+	 * @param n
+	 * @param consumer
+	 */
+	public static <T> void apply(Domain<T> domain, Predicate<T> condition, Consumer<T> consumer) {
+		// FIXME: this is a problem which needs to be fixed!
+		final int size = (int) domain.size();
+		//
+		for(int i=0;i!=size;++i) {
+			T item = domain.get(i);
+			if(condition.test(item)) {
+				consumer.accept(item);
+			}
+		}
+	}
 
 	/**
 	 * Return a domain which generates lists with between zero and <code>n</code>
@@ -44,6 +63,40 @@ public class Domains {
 			@Override
 			public java.util.List<T> generate(java.util.List<T> items) {
 				return items;
+			}
+		};
+	}
+
+	/**
+	 * Generators
+	 * @param domain
+	 * @param constraint
+	 * @return
+	 */
+	public static <T> Domain<T> Constrained(Domain<T> domain, Predicate<T> constraint) {
+		// First identify matching items
+		ArrayList<Long> tmp = new ArrayList<>();
+		for(long i=0;i!=domain.size();++i) {
+			T item = domain.get(i);
+			if(constraint.test(item)) {
+				tmp.add(i);
+			}
+		}
+		// Stash indices
+		final long[] indices = new long[tmp.size()];
+		for(int i=0;i!=tmp.size();++i) {
+			indices[i] = tmp.get(i);
+		}
+		// Done
+		return new AbstractDomain<T>() {
+			@Override
+			public long size() {
+				return indices.length;
+			}
+
+			@Override
+			public T get(long index) {
+				return domain.get(indices[(int) index]);
 			}
 		};
 	}
